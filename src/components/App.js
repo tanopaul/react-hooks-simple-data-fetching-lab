058cadb871f18cbb834493dcc51bf39fd5ef1365
Tano Paul <tanopaul38@gmail.com>
2023-08-25 15:18:22 -0400

    completed simple fetching lab hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [loaded, setLoaded] = useState(false);
+    const [image, setImage] = useState('');
+
+    const url = 'https://dog.ceo/api/breeds/image/random';
+
+    useEffect(() => {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(image => {
+            setImage(image);
+            setLoaded(true);
+        })
+    }, [])
+
+
+
+    return (
+        <div>
+            {!loaded ? <p>Loading...</p> : <img src={image.message} alt="A Random Dog"/>}
+        </div>
+    )
+    
+}
+
+export default App;
+
